4b113b05cf172edebdce5414b76e0902ace2f6c5
chiammin97 <chiammin@gmail.com>
2020-06-14 00:03:09 +0800

    feature: user is able to adjust session and break time

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const $inputTask = document.getElementById("inputTask");
 const $form = document.getElementById("form");
 const $submit = document.getElementById("submit");
 const $alert = document.getElementById("alert");
-const $removeBtn = document.getElementById("button-remove");
-const $editBtn = document.getElementById("button-edit");
+
+//To Do List
 
 loadEventListeners();
 
@@ -30,18 +30,39 @@ function addToDo(e) {
     //li.innerHTML = `<li>${$inputTask.value}</li>`;
     li.innerHTML = `
     <p class="text">${text}</p>
-    <span id="button-edit-${id}" class="material-icons">edit</>
-    <span id="button-remove-${id}">
-      <i class="material-icons">remove_circle</i>
-    </span>
+    <div id="taskListBtn">
+      <span id="button-tick-${id}" class="material-icons tick-style">check_circle</span>
+      <span id="button-edit-${id}" class="material-icons" edit-style>edit</span>
+      <span id="button-remove-${id}" class="material-icons remove-style">delete</span>
+    </div>
     `;
+
     console.log(li);
     $list.appendChild(li);
     $inputTask.value = "";
 
+    //Done Button
+    const $tickBtn = document.getElementById(`button-tick-${id}`);
+    $tickBtn.addEventListener("click", function (done) {
+      console.log("Marked as Done");
+      li.classList.remove("item");
+      li.classList.add("item-done");
+      li.innerHTML = `
+      <p class="text-done">${text}</p>
+      <div id="taskListBtn">
+        <span id="button-tick-${id}" class="material-icons tick-style">check_circle</span>
+        <span id="button-edit-${id}" class="material-icons" edit-style>edit</span>
+        <span id="button-remove-${id}" class="material-icons remove-style">delete</span>
+      </div>
+      `;
+    });
+
+    //Edit Button
+
+    //Remove Button
     const $removeBtn = document.getElementById(`button-remove-${id}`);
-    $removeBtn.addEventListener("click", function (e) {
-      const confirm = window.confirm("Are you sure?");
+    $removeBtn.addEventListener("click", function (remove) {
+      const confirm = window.confirm("Delete Task?");
 
       if (confirm) {
         li.remove();
@@ -58,3 +79,53 @@ function showAlert() {
     $alert.style.visibility = "hidden";
   }, 2000);
 }
+
+//Clock Settings
+const $sessionMinus = document.getElementById("sessionMinus");
+const $sessionPlus = document.getElementById("sessionPlus");
+const $sessionTime = document.getElementById("sessionTime");
+const $breakMinus = document.getElementById("breakMinus");
+const $breakPlus = document.getElementById("breakPlus");
+const $breakTime = document.getElementById("breakTime");
+
+//Clock Time
+const $min = document.getElementById("minutes");
+const $sec = document.getElementById("seconds");
+
+//Clock Buttons
+const $clockStart = document.getElementById("clock-start");
+const $clockPause = document.getElementById("clock-pause");
+const $clockStop = document.getElementById("clock-stop");
+const $clockReset = document.getElementById("clock-reset");
+
+decreaseSession = 25;
+$sessionMinus.onclick = function (e) {
+  if (decreaseSession > 15) {
+    decreaseSession -= 1;
+    $sessionTime.innerHTML = decreaseSession;
+  }
+};
+
+increaseSession = 25;
+$sessionPlus.onclick = function (e) {
+  if (increaseSession < 60) {
+    increaseSession += 1;
+    $sessionTime.innerHTML = increaseSession;
+  }
+};
+
+decreaseBreak = 5;
+$breakMinus.onclick = function (e) {
+  if (decreaseBreak > 0) {
+    decreaseBreak -= 1;
+    $breakTime.innerHTML = decreaseBreak;
+  }
+};
+
+increaseBreak = 5;
+$breakPlus.onclick = function (e) {
+  if (increaseBreak < 30) {
+    increaseBreak += 1;
+    $breakTime.innerHTML = increaseBreak;
+  }
+};
